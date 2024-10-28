7dddecae74138468d084d8ecd355818c416385b0
Vladislav <vladtaranov095@gmail.com>
2024-10-27 19:40:04 -0800

    third_testing_of_matrix_

--- tests/intro.Matrix.test.js
+++ tests/intro.Matrix.test.js
@@ -1,35 +1,63 @@
 const { checkStatus } = require("../app/originalMatrix")
 
-describe('test_of_matrix', () => {
+describe('test 1x1', () => {
     let matrix
-    matrix = checkStatus(6, 3)
-
-    describe('Test lenght and char of Matrix', () => {
-        test('Test rows', () => {
-            let numRows = matrix.split("\n")
-            let rows = numRows.length
-            expect(rows).toBe(7)
-        })
-        test('Test columns', () => {
-            let numRows = matrix.split("\n")
-            let rows = numRows.length
-            let columns = numRows[0].split(" ").length
-            expect(columns).toBe(4)
-        })
-        test('Char', () => {
-
-        })
+    matrix = checkStatus(1, 1)
+    test('Test rows', () => {
+        let numRows = matrix.split("\n")
+        let rows = numRows.length
+        expect(rows).toBe(2)
     })
-    describe('', () => {
-
-
-
-
-        
+    test('Test columns', () => {
+        let numRows = matrix.split("\n")
+        let rows = numRows.length
+        let columns = numRows[0].split(" ").length
+        expect(columns).toBe(2)
     })
+    test('Char', () => {
 
-
-
-
-
+    })
 })
+
+
+describe('test 99x99', () => {
+    let matrix
+    matrix = checkStatus(99, 99)
+    test('Test rows', () => {
+        let numRows = matrix.split("\n")
+        let rows = numRows.length
+        expect(rows).toBe(100)
+    })
+    test('Test columns', () => {
+        let numRows = matrix.split("\n")
+        let rows = numRows.length
+        let columns = numRows[0].split(" ").length
+        expect(columns).toBe(100)
+    })
+    test('Char', () => {
+
+    })
+})
+
+
+
+describe('test 12x17', () => {
+    let matrix
+    matrix = checkStatus(12, 17)
+    test('Test rows', () => {
+        let numRows = matrix.split("\n")
+        let rows = numRows.length
+        expect(rows).toBe(13)
+    })
+    test('Test columns', () => {
+        let numRows = matrix.split("\n")
+        let rows = numRows.length
+        let columns = numRows[0].split(" ").length
+        expect(columns).toBe(18)
+    })
+    test('Char', () => {
+
+    })
+})
+
+
