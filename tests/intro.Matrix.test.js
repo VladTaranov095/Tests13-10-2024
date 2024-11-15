a585f32af4d10e780abb1195e7b0f327fd197e89
Vladislav <vladtaranov095@gmail.com>
2024-11-14 18:36:34 -0800

    tests_of_matrix

--- tests/intro.Matrix.test.js
+++ tests/intro.Matrix.test.js
@@ -4,13 +4,15 @@ describe('test 1x1', () => {
     let matrix
     matrix = checkStatus(1, 1)
     test('Test rows', () => {
-        let rows = matrix.split("\n", 1)
-        let numRows = rows.length
+        let splitRows = matrix.split("\n")
+        let rows = splitRows
+        let numRows = rows.filter(element => element !== "" ).length
         expect(numRows).toBe(1)
     })
     test('Test columns', () => {
-        let rows = matrix.split("\n")
-        let numColumns = rows[0].split(" ", 1).length
+        let splitRows = matrix.split("\n")
+        let splitColumns = splitRows[0].split(" ")
+        let numColumns = splitColumns.filter(element => element !== "" ).length
         expect(numColumns).toBe(1)
     })
     test('Char', () => {
@@ -21,13 +23,15 @@ describe('test 99x99', () => {
     let matrix
     matrix = checkStatus(99, 99)
     test('Test rows', () => {
-        let rows = matrix.split("\n", 99)
-        let numRows = rows.length
+        let splitRows = matrix.split("\n")
+        let rows = splitRows
+        let numRows = rows.filter(element => element !== "" ).length
         expect(numRows).toBe(99)
     })
     test('Test columns', () => {
-        let rows = matrix.split("\n")
-        let numColumns = rows[0].split(" ", 99).length
+        let splitRows = matrix.split("\n")
+        let splitColumns = splitRows[0].split(" ")
+        let numColumns = splitColumns.filter(element => element !== "" ).length
         expect(numColumns).toBe(99)
     })
     test('Char', () => {
@@ -37,16 +41,17 @@ describe('test 12x17', () => {
     let matrix
     matrix = checkStatus(12, 17)
     test('Test rows', () => {
-        let rows = matrix.split("\n", 12)
-        let numRows = rows.length
+        let splitRows = matrix.split("\n")
+        let rows = splitRows
+        let numRows = rows.filter(element => element !== "" ).length
+
         expect(numRows).toBe(12)
     })
     test('Test columns', () => {
         let rows = matrix.split("\n")
-        let numColumns = rows[0].split(" ", 17).length
+        let splitColumns = rows[0].split(" ")
+        let numColumns = splitColumns.filter(element => element !== "" ).length
         expect(numColumns).toBe(17)
-    })
-    test('Char', () => {
     })
 })
 describe('test char of matrix', () => {
@@ -54,7 +59,8 @@ describe('test char of matrix', () => {
         let matrix
         matrix = checkStatus(3, 3)
         let ab = matrix.split("\n")
-        let char = ab[0].split(" ", 3)
+        let char = ab[0].split(" ")
+
         expect(char).toContain("a")
     })
 })
@@ -62,8 +68,10 @@ describe('test 3x3', () => {
     let matrix
     matrix = checkStatus(3, 3)
     test('Test rows', () => {
-        let rows = matrix.split("\n", 3)
-        let numRows = rows.length
+        let splitRows = matrix.split("\n")
+        let rows = splitRows
+        let numRows = rows.filter(element => element !== "" ).length
+
         expect(numRows).toBe(3)
     })
     test('Test columns', () => {
