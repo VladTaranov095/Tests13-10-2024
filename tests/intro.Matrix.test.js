c67fb12d26965e2a48d877e16e2bcd7d2be4a524
Vladislav <vladtaranov095@gmail.com>
2024-12-31 16:31:40 +0300

    usage dry

--- tests/intro.Matrix.test.js
+++ tests/intro.Matrix.test.js
@@ -1,6 +1,37 @@
 const { checkStatus } = require("../app/originalMatrix")
 const constructionElement = "a"
 
+
+describe.only.each([
+    {matrix: checkStatus(1,1), expectedRows: 1, expectedColumns: 1, expectedChar: "a"},
+    {matrix: checkStatus(99,99), expectedRows: 99, expectedColumns: 99, expectedChar: "a"},
+    {matrix: checkStatus(12,17), expectedRows: 12, expectedColumns: 17, expectedChar: "a"},
+    {matrix: checkStatus(3,3), expectedRows: 3, expectedColumns: 3, expectedChar: "a"}
+])('Test matrix: 1x1, 99x99, 12x17, 3x3', ({matrix, expectedRows, expectedColumns, expectedChar}) => {
+    test(`A test for matching the number of rows received to the expected ${expectedRows}.`, () => {
+        let rows = matrix.split("\n").filter(element => element !== '');
+        let numRows = rows.filter(element => element !== "").length
+        expect(numRows).toBe(expectedRows)
+    })
+    test(`A test for matching the number of columns received to the expected ${expectedColumns}.`, () => {
+        let rows = matrix.split("\n").filter(element => element !== '');
+        rows.forEach(row => {
+            splitColumns = row.split(" ");
+            numColumns = splitColumns.filter(element => element !== '').length
+            expect(numColumns).toBe(expectedColumns)
+        })
+        })
+        test('Char "a"', () => {
+            let rows = matrix.split("\n").filter(element => element !== '');
+                    rows.forEach(row => {
+                        splitColumns = row.split(" ");
+                        numColumns = splitColumns.filter(element => element !== '');
+                        let char = numColumns.some(chars => chars !== constructionElement)
+                        expect(char).toBeFalsy()
+                    })
+})
+})
+
 describe('test 1x1', () => {
     let matrix
     matrix = checkStatus(1, 1)
